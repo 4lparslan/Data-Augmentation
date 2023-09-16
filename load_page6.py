7d603e7c3108968f0fb177f86fd6c338dc7f6f78
Alparslan İdris Arslan <46932194+4lparslan@users.noreply.github.com>
2023-09-16 16:35:04 +0300

    Delete load_page6.py

--- load_page6.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import *
-from page6 import Ui_Form
-
-class Page6(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.p6 = Ui_Form()
-        self.p6.setupUi(self)
-
