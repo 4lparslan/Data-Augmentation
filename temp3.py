c3689cfc90cf9a011ad73159accd35475d4c01cb
Alparslan İdris Arslan <46932194+4lparslan@users.noreply.github.com>
2023-09-16 16:26:12 +0300

    Delete temp3.py

--- temp3.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dize = "/home/alp/Desktop/dataset/1.jpg"  # Örnek bir dize
-dosya_yolu = os.path.abspath(dize)  # Dizeyi mutlak bir dosya yoluna dönüştürün
-
-if os.path.exists(dosya_yolu):
-    print("Doğru bir dosya yolu:", dosya_yolu)
-else:
-    print("Geçersiz bir dosya yolu:", dosya_yolu)
